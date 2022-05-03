3e0e9ffd1ff68793859eafc88fe9773519d72e57
zubayer <zubayer3570@gmail.com>
2022-05-03 23:25:22 +0600

    'test'

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
 //middleware
 app.use(cors())
 app.use(express.json())
@@ -11,5 +13,22 @@ const port = process.env.PORT || 5000
 app.get('/', (req, res) => {
     res.send('server is working fine')
 })
+//mongodb
+const uri = `mongodb+srv://database-user-1:${process.env.DATABASE_PASSWORD}@cluster0.1f3iy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const client = new MongoClient(uri)
+const run = () => {
+    try {
+        client.connect()
+        app.get('/items', async (req, res) => {
+            const collection = client.db('warehouse').collection('car-collection')
+            const cursor = collection.find({})
+            const cars = await cursor.toArray()
+            res.send(cursor)
+        })
+    } finally {
+
+    }
+}
+run()
 //listen
 app.listen(port)
