dd6682da94a3f7c23bb0c92c45ee55fa32384e21
zubayer <zubayer3570@gmail.com>
2022-05-04 17:45:11 +0600

    'commit'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 require('dotenv').config()
 //middleware
 app.use(cors())
@@ -19,11 +19,36 @@ const client = new MongoClient(uri)
 const run = () => {
     try {
         client.connect()
+        const collection = client.db('warehouse').collection('car-collection')
         app.get('/items', async (req, res) => {
-            const collection = client.db('warehouse').collection('car-collection')
+            const cursor = collection.find({}).limit(6)
+            const cars = await cursor.toArray()
+            res.send(cars)
+        })
+        app.get('/allitems', async (req, res) => {
             const cursor = collection.find({})
             const cars = await cursor.toArray()
             res.send(cars)
+        })
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {
+                _id: ObjectId(id)
+            }
+            const result = await collection.findOne(query)
+            res.send(result)
+        })
+        app.post('/inventory/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {
+                _id: ObjectId(id)
+            }
+            const itemToUpdate = await collection.findOne(query)
+            const result = await collection.updateOne(query, {
+                $set: { quantity: itemToUpdate.quantity - 1 }
+            })
+            res.send(result)
+
         })
     } finally {
 
