4ce2bc75d69811203896e7ff6b8596cbd8b962fc
zubayer <zubayer3570@gmail.com>
2022-05-08 10:50:24 +0600

    commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 const { MongoClient, ObjectId } = require('mongodb')
 require('dotenv').config()
 //middleware
@@ -14,12 +15,12 @@ app.get('/', (req, res) => {
     res.send('server is working fine')
 })
 //mongodb
-const uri = `mongodb+srv://database-user-1:databaseofzubayer@cluster0.1f3iy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const uri = `mongodb+srv://database-user-1:${process.env.DATABASE_PASSWORD}@cluster0.1f3iy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri)
 const run = () => {
     try {
         client.connect()
-        const collection = client.db('warehouse').collection('car-collection')
+        const collection = client.db('watch-geek').collection('watches')
         //get
         app.get('/items/:amount', async (req, res) => {
             const amount = req.params.amount
@@ -60,6 +61,7 @@ const run = () => {
             await collection.updateOne(query, {
                 $set: { quantity: newQuantity }
             })
+            res.send({})
         })
         app.post('/delete', async (req, res) => {
             const query = {
@@ -68,19 +70,22 @@ const run = () => {
             await collection.deleteOne(query)
         })
         app.post('/add-item', async (req, res) => {
-            const doc = req.body;
-            const result = await collection.insertOne(doc)
-            console.log(result.insertedId)
-            res.send(result.insertedId)
+            const { doc } = req.body;
+            await collection.insertOne(doc)
         })
         app.post('/my-items', async (req, res) => {
             const { email } = req.body;
-            const query = {
-                email: email
-            }
+            const query = { email }
             const cursor = collection.find(query)
             const result = await cursor.toArray()
             res.send(result)
+        })
+        app.post('/login', (req, res) => {
+            const email = req.body;
+            const accessToken = jwt.sign({ email }, process.env.SECRET_KEY, {
+                expiresIn: '1h'
+            })
+            res.send({ accessToken })
         })
     } finally { }
 }
